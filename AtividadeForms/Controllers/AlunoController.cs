a1b06dd0597fd99c6b4f855c825f4cd5457f2e7a
PEDRONASCII <nascimentopedroryan@uni9.edu.br>
2026-06-08 18:57:13 +0000

    Formulário de Cadastro de Aluno com validação

---
+++ AtividadeForms/Controllers/AlunoController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class AlunoController : Controller
+{
+    [HttpGet]
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar(Aluno aluno)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(aluno);
+        }
+
+        return View("Confirmacao", aluno);
+    }
+}
